b6a53a6d2012818d18d2907650c2bb5435e06946
naoiwata <nao@bascule.co.jp>
2013-03-03 18:24:42 +0900

    solved : 1.31

--- chapter1/q-1.30.scm
+++ chapter1/q-1.30.scm
@@ -7,7 +7,7 @@
 (define (sum term a next b)
 	(define (iter a result))
 	(if (> a b)
-		0
+		result
 		(iter (next a) (+ (term a) result)))
 	(iter a 0))
 
--- chapter1/q-1.31.scm
+++ chapter1/q-1.31.scm
@@ -4,4 +4,50 @@
 ;; q-1.31
 ;;
 
+(add-load-path "." :relative)
+(load "lib.scm")
 
+; (a)
+(define (product term a next b)
+	(if (> a b)
+		1
+		(*
+			(term a)
+			(product term (next a) next b))))
+
+(define (inc a)
+	(+ a 1))
+
+; factorical
+(define (factorical n)
+	(define (multi a)
+		a)
+	(product multi 1 inc n))
+
+(print 
+	(factorical 6))
+; -> 720
+
+; Wallis product
+(define (wallis-pi n)
+	(define (f x)
+		(/
+			(* 4.0 (square x))
+			(- (* 4.0 (square x)) 1)))
+	(* 
+		2.0
+		(product f 1 inc n)))
+
+(print
+	(wallis-pi 999999))
+; -> 3.1415918681810604
+
+; (b)
+(define (product-i term a next b)
+	(define (iter a result))
+	(if (> a b)
+		result
+		(iter (next a) (* (term a) result)))
+	(iter a 0))
+
+; END
