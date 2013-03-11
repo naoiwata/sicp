8365afb135bd670d040194132a7208db88ab0885
naoiwata <nao@bascule.co.jp>
2013-03-11 09:48:29 +0900

    solved: 2.1~2.3 :P

--- chapter2/pages/lib.scm
+++ chapter2/pages/lib.scm
@@ -4,6 +4,12 @@
 ;; lib on chapter2
 ;;
 
+; abs
+(define (abs n)
+	(if (< n 0)
+		(- 0 n)
+		n))
+
 ; average
 (define (average a b)
 	(/
--- chapter2/q2.3.scm
+++ chapter2/q2.3.scm
@@ -5,5 +5,69 @@
 ;;
 
 (add-load-path "." :relative)
-(load "q.2.2.scm")
+(load "q2.2.scm")
+(load "pages/lib.scm")
 
+; rectangle
+(define (rectangle p q)
+	(cons p q))
+
+; abstruct these procedure
+(define (abst-2points f r)
+	(let
+		((p (start-segment r))
+		(q (end-segment r)))
+		(f p q)))
+
+; test : midpoint-segment
+(define p1 (make-point 2 6))
+(define p2 (make-point 4 10))
+(define r1 (make-segment p1 p2))
+
+(print-point 
+	(abst-2points
+		(lambda (p q)
+			(make-point
+				(average (x-point p) (x-point q))
+				(average (y-point p) (y-point q))))
+		r1))
+;(3, 8) -> ok
+
+; permineter
+(define (permineter r)
+	(abst-2points
+		(lambda (p q)
+			(*
+				2
+				(+
+					(abs
+						(-
+							(x-point p)
+							(x-point q)))
+					(abs (-
+							(y-point p)
+							(y-point q))))))
+		r))
+
+; test
+(print (permineter r1))
+; 12 -> ok
+
+; area
+(define (area r)
+	(abst-2points
+		(lambda (p q)
+			(*
+				(abs
+					(-
+						(x-point p)
+						(x-point q)))
+				(abs (-
+						(y-point p)
+						(y-point q)))))
+		r))
+; test
+(print (area r1))
+; 8 -> ok
+
+; END
