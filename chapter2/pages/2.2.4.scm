f80aabc759fbf9844bca468fc293492398073e5a
naoiwata <nao@bascule.co.jp>
2013-03-22 00:24:47 +0900

    solved: q2.50

--- chapter2/pages/2.2.4.scm
+++ chapter2/pages/2.2.4.scm
@@ -95,4 +95,64 @@ rotate180 flip-vert)))
 					((frame-coord-map frame) (end-segment segment))))
 			segment-list)))
 
+;; Transforming and combining painters ;;
+
+(define (transform-painter painter origin corner1 corner2)
+	(lambda (frame)
+		(let 
+			((m (frame-coord-map frame)))
+			(let 
+				((new-origin (m origin)))
+				(painter
+					(make-frame new-origin
+						(sub-vect (m corner1) new-origin)
+						(sub-vect (m corner2) new-origin)))))))
+
+; flip painter images vertically:
+(define (flip-vert painter)
+	(transform-painter painter
+		(make-vect 0.0 1.0) ; new origin
+		(make-vect 1.0 1.0) ; new end of edge1
+		(make-vect 0.0 0.0))) ; new end of edge2
+
+; painter that shrinks its image to the upper-right quarter of the frame
+(define (shrink-to-upper-right painter)
+	(transform-painter painter
+		(make-vect 0.5 0.5)
+		(make-vect 1.0 0.5)
+		(make-vect 0.5 1.0)))
+
+; rotate images counterclockwise by 90 degrees
+(define (rotate90 painter)
+	(transform-painter painter
+		(make-vect 1.0 0.0)
+		(make-vect 1.0 1.0)
+		(make-vect 0.0 0.0)))
+
+; squash images towards the center of the frame
+(define (squash-inwards painter)
+	(transform-painter painter
+		(make-vect 0.0 0.0)
+		(make-vect 0.65 0.35)
+		(make-vect 0.35 0.65)))
+
+; beside
+(define (beside painter1 painter2)
+	(let 
+		((split-point (make-vect 0.5 0.0)))
+		(let 
+			((paint-left
+				(transform-painter painter1
+					(make-vect 0.0 0.0)
+					split-point
+					(make-vect 0.0 1.0)))
+			(paint-right
+				(transform-painter painter2
+				split-point
+				(make-vect 1.0 0.0)
+				(make-vect 0.5 1.0))))
+			(lambda (frame)
+				(paint-left frame)
+				(paint-right frame)))))
+
 ; END
---
+++ chapter2/q2.50.scm
@@ -0,0 +1,23 @@
+;; @author naoiwata
+;; SICP Chapter2
+;; question 2.50
+
+(define (flip-horiz painter)
+	(transform-painter painter
+		(make-vect 1.0 0.0)
+		(make-vect 0.0 0.0)
+		(make-vect 1.0 1.0))))
+
+(define (rotate-180 painter)
+	(transform-painter painter
+		(make-vect 1.0 1.0)
+		(make-vect 0.0 1.0)
+		(make-vect 1.0 0.0)))))
+
+(define (rotate-270 painter)
+	(transform-painter painter
+		(make-vect 0.0 1.0)
+		(make-vect 0.0 0.0)
+		(make-vect 1.0 1.0)))))
+
+; END
