9ca10797814c6d57a4d58382732d4c267014a31d
naoiwata <nao@bascule.co.jp>
2013-03-24 02:09:13 +0900

    solved 2.58

---
+++ chapter2/q2.58.scm
@@ -0,0 +1,47 @@
+;; @author naoiwata
+;; SICP Chapter2
+;; question 2.58
+
+(add-load-path "." :relative)
+(load "q2.57.scm")
+
+(define (sum? x)
+	(and (pair? x) (eq? (cadr x) '+)))
+
+(define (addend s) 
+	(car s))
+
+(define (product? x)
+	(and (pair? x) (eq? (cdr x) '*)))
+
+(define (multiplier p) 
+	(car p))
+
+(define (make-sum a1 a2)
+	(cond 
+		((=number? a1 0) 
+			a2)
+		((=number? a2 0) 
+			a1)
+		((and (number? a1) (number? a2)) 
+			(+ a1 a2))
+		(else 
+			(list a1 '+ a2))))
+
+(define (make-product m1 m2)
+	(cond 
+		((or (=number? m1 0) (=number? m2 0))
+			0)
+		((=number? m1 1) 
+			m2)
+		((=number? m2 1) 
+			m1)
+		((and (number? m1) (number? m2)) 
+			(* m1 m2))
+		(else 
+			(list m1 '* m2))))
+; test
+(print '(x + (3 * (x + (y + 2)))))
+; (x + (3 * (x + (y + 2))))
+
+; END
