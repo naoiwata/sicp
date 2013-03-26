1966fd70daa73743bd3b5303f323d64c6b756211
naoiwata <nao@bascule.co.jp>
2013-03-27 02:11:15 +0900

    solved: q2.63

--- chapter2/pages/2.3.3.scm
+++ chapter2/pages/2.3.3.scm
@@ -58,5 +58,45 @@
 				((< x2 x1)
 					(intersection-set set1 (cdr set2)))))))
 
+;; Sets as binary trees ;;
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (car (cdr tree)))
+
+(define (right-branch tree)
+  (car (cdr (cdr tree))))
+
+(define (make-tree entry left-branch right-branch)
+  (list entry left-branch right-branch))
+
+(define (element-of-set? x set)
+  (cond
+    ((null? set)
+     #f)
+    ((= x (entry set))
+     #t)
+    ((< x (entry set))
+     (element-of-set? x (left-branch set)))
+    ((< (entry set) x)
+     (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond 
+    ((null? set) 
+     (make-tree x '() '()))
+    ((= x (entry set)) 
+     set)
+    ((< x (entry set))
+     (make-tree 
+       (entry set)
+       (adjoin-set x (left-branch set))
+       (right-branch set)))
+    ((> x (entry set))
+     (make-tree 
+       (entry set)
+       (left-branch set)
+       (adjoin-set x (right-branch set))))))
 
 ; END
---
+++ chapter2/q2.63.scm
@@ -0,0 +1,53 @@
+;; @author naoiwata
+;; SICP Chapter2
+;; question 2.63
+
+(add-load-path "." :relative)
+(load "pages/2.3.3.scm")
+
+(define (tree->list-1 tree)
+  (if (null? tree)
+      '()
+      (append 
+        (tree->list-1 (left-branch tree))
+        (cons 
+          (entry tree)
+          (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+        result-list
+        (copy-to-list 
+          (left-branch tree)
+          (cons 
+            (entry tree)
+            (copy-to-list 
+              (right-branch tree)
+              result-list)))))
+  (copy-to-list tree '()))
+
+; (a)
+; test
+(define tree-a '(7 (3 (1 () ()) (5 () ())) (9 () (11 () ()))))
+(print (tree->list-1 tree-a)) ; (1 3 5 7 9 11)
+(print (tree->list-2 tree-a)) ; (1 3 5 7 9 11)
+
+(define tree-b '(3 (1 () ()) (7 (5 () ()) (9 () (11 () ()))))) 
+(print (tree->list-1 tree-b)) ; (1 3 5 7 9 11)
+(print (tree->list-2 tree-b)) ; (1 3 5 7 9 11)
+
+(define tree-b '(5 (3 (1 () ()) ()) (9 (7 () ()) (11 () ()))))
+(print (tree->list-1 tree-b)) ; (1 3 5 7 9 11)
+(print (tree->list-2 tree-b)) ; (1 3 5 7 9 11)
+; these results are same.
+
+; (b)
+#|
+tree->list-1 
+	method: Linear Recursion
+	step:   log(n) * append procedure = log(n) * n
+tree->list-2 
+	method: Iteration
+	step:   log(n)
+|#
