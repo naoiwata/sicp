7f4f63d87a2f7c20e8aa167a55b275dc7385af7c
naoiwata <nao@bascule.co.jp>
2013-03-31 00:07:29 +0900

    solved: a2.72 Huffman Encoding Trees has end

---
+++ chapter2/q2.72.scm
@@ -0,0 +1,18 @@
+;; @author naoiwata
+;; SICP Chapter2
+;; question 2.72
+
+#|
+n = 1 : Θ(1)
+n = 2 : Θ(2)
+n = 3 : Θ(4)
+n = 4 : Θ(8)
+n = 5 : Θ(16)
+
+(1) most frequent symbols in the alphabet: the order of growth
+Θ(n^2)
+
+(2) least frequent symbols in the alphabet: the order of growth
+Θ(1)
+
+|#
