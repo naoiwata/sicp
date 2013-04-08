132784b63b7a6001eff0449874eab870b24f11a6
naoiwata <nao@bascule.co.jp>
2013-04-08 12:35:33 +0900

    solved? 2.77

--- chapter2/q2.77.scm
+++ chapter2/q2.77.scm
@@ -2,7 +2,7 @@
 ;; SICP Chapter2
 ;; question 2.77
 
-; generic settings -------------------------------------------------
+; utility settings -------------------------------------------------
 
 (define (attach-tag type-tag contents)
   (cons type-tag contents))
@@ -23,10 +23,10 @@
     (let 
       ((proc (get op type-tags)))
       (if proc
-          (apply proc (map  args))
+          (apply proc (map contents args))
           (error
-            "No method for these types -- APPLY-GENERIC"
-            (list op type-tags))))))
+           "No method for these types -- APPLY-GENERIC"
+           (list op type-tags))))))
 
 (define (gcd a b)
   (if (= b 0)
@@ -37,6 +37,7 @@
 
 ; put and get procedures -------------------------------------------
 
+#|
 (define operation-table (make-hash-table))
 
 (define (put op type item)
@@ -57,6 +58,42 @@
       ((type-table (hash-table-get operation-table type)))
       (hash-table-get type-table op))
     #f))
+|#
+
+; thanks to -> http://karetta.jp/book-node/YetAnotherSICPAnswerBook/251301
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key-1 key-2)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  #f))
+            #f)))
+    (define (insert! key-1 key-2 value)
+      (let ((subtable (assoc key-1 (cdr local-table))))
+        (if subtable
+            (let ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! local-table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr local-table)))))
+      'ok)    
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else (error "Unknown operation -- TABLE" m))))
+    dispatch))
+
+(define operation-table (make-table))
+(define get (operation-table 'lookup-proc))
+(define put (operation-table 'insert-proc!))
 
 ; ordinary procedure -----------------------------------------------
 
@@ -130,7 +167,7 @@
              (square (imag-part z)))))
   (define (angle z)
     (atan (imag-part z) (real-part z)))
-  (define (make-from-mag-ang r a)
+  (define (make-from-imag-ang r a)
     (cons (* r (cos a)) (* r (sin a))))
   ;; interface to the rest of the system
   (define (tag x) (attach-tag 'rectangular x))
@@ -140,8 +177,8 @@
   (put 'angle '(rectangular) angle)
   (put 'make-from-real-imag 'rectangular
        (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'rectangular
-       (lambda (r a) (tag (make-from-mag-ang r a))))
+  (put 'make-from-imag-ang 'rectangular
+       (lambda (r a) (tag (make-from-imag-ang r a))))
   'done)
 
 (define (make-from-real-imag x y)
@@ -153,7 +190,7 @@
   ;; internal procedures
   (define (magnitude z) (car z))
   (define (angle z) (cdr z))
-  (define (make-from-mag-ang r a) (cons r a))
+  (define (make-from-imag-ang r a) (cons r a))
   (define (real-part z)
     (* (magnitude z) (cos (angle z))))
   (define (imag-part z)
@@ -169,12 +206,12 @@
   (put 'angle '(polar) angle)
   (put 'make-from-real-imag 'polar
        (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'polar
-       (lambda (r a) (tag (make-from-mag-ang r a))))
+  (put 'make-from-imag-ang 'polar
+       (lambda (r a) (tag (make-from-imag-ang r a))))
   'done)
 
-(define (make-from-mag-ang r a)
-  ((get 'make-from-mag-ang 'polar) r a))
+(define (make-from-imag-ang r a)
+  ((get 'make-from-imag-ang 'polar) r a))
 
 ; complex procedure ---------------------------------------------
 
@@ -182,8 +219,8 @@
   ;; imported procedures from rectangular and polar packages
   (define (make-from-real-imag x y)
     ((get 'make-from-real-imag 'rectangular) x y))
-  (define (make-from-mag-ang r a)
-    ((get 'make-from-mag-ang 'polar) r a))
+  (define (make-from-imag-ang r a)
+    ((get 'make-from-imag-ang 'polar) r a))
   ;; internal procedures
   (define (add-complex z1 z2)
     (make-from-real-imag (+ (real-part z1) (real-part z2))
@@ -192,11 +229,21 @@
     (make-from-real-imag (- (real-part z1) (real-part z2))
                          (- (imag-part z1) (imag-part z2))))
   (define (mul-complex z1 z2)
-    (make-from-mag-ang (* (magnitude z1) (magnitude z2))
+    (make-from-imag-ang (* (magnitude z1) (magnitude z2))
                        (+ (angle z1) (angle z2))))
   (define (div-complex z1 z2)
-    (make-from-mag-ang (/ (magnitude z1) (magnitude z2))
+    (make-from-imag-ang (/ (magnitude z1) (magnitude z2))
                        (- (angle z1) (angle z2))))
+  ;; ------- add from -------
+  (define (real-part z)
+    (apply-generic 'real-part z))
+  (define (imag-part z)
+    (apply-generic 'imag-part z))
+  (define (magnitude z)
+    (apply-generic 'magnitude z))
+  (define (angle z)
+    (apply-generic 'angle z))
+  ;; ------- add end -------
   ;; interface to rest of the system
   (define (tag z) (attach-tag 'complex z))
   (put 'add '(complex complex)
@@ -209,8 +256,8 @@
        (lambda (z1 z2) (tag (div-complex z1 z2))))
   (put 'make-from-real-imag 'complex
        (lambda (x y) (tag (make-from-real-imag x y))))
-  (put 'make-from-mag-ang 'complex
-       (lambda (r a) (tag (make-from-mag-ang r a))))
+  (put 'make-from-imag-ang 'complex
+       (lambda (r a) (tag (make-from-imag-ang r a))))
   ;; ------- add from -------
   (put 'real-part '(complex) real-part)
   (put 'imag-part '(complex) imag-part)
@@ -223,7 +270,7 @@
   ((get 'make-from-real-imag 'complex) x y))
 
 (define (make-complex-from-mag-ang r a)
-  ((get 'make-from-mag-ang 'complex) r a))
+  ((get 'make-from-imag-ang 'complex) r a))
 
 ; test --------------------------------------------------------
 
@@ -238,23 +285,100 @@
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
 
-(define z (make-complex-from-real-imag 3 4))
-(print magnitude) 
-; (complex rectangular 3 . 4)
+; test
+(define z 
+  (make-complex-from-real-imag 3 4))
+; -> (complex rectangular 3 . 4)
 
-(magnitude z) 
-; -> *** ERROR: No method for these types -- APPLY-GENERIC (magnitude (complex))
+(define (magnitude x) 
+  (apply-generic 'magnitude x))
+
+(magnitude z)
+; 5
 
 #|
-;; ------- add from -------
-  (define (real-part z)
-    (apply-generic 'real-part z))
-  (define (imag-part z)
-    (apply-generic 'imag-part z))
-  (define (magnitude z)
-    (apply-generic 'magnitude z))
-  (define (angle z)
-    (apply-generic 'angle z))
-  ;; ------- add end -------
-  \
+(magnitude z) 
+->
+(magnitude (make-complex-from-real-imag 3 4))
+->
+(magnitude ((get 'make-from-real-imag 'complex) 3 4))
+->
+(magnitude ((lambda (x y) (tag (make-from-real-imag x y))) 3 4))
+->
+(magnitude ((lambda (x y) (cons 'complex (make-from-real-imag x y))) 3 4))
+->
+(magnitude ((lambda (x y) (cons 'complex (cons 'rectangular (cons x y)))) 3 4))
+->
+(magnitude (cons 'complex (cons 'rectangular (cons 3 4))))
+->
+(apply-generic 'magnitude (cons 'complex (cons 'rectangular (cons 3 4))))
+->
+  (let 
+    ((type-tags (map type-tag (cons 'complex (cons 'rectangular (cons 3 4))))))
+    (let 
+      ((proc (get 'magnitude type-tags)))
+      (if proc
+          (apply proc (map contents (cons 'complex (cons 'rectangular (cons 3 4)))))
+          (error
+            "No method for these types -- APPLY-GENERIC"
+            (list 'magnitude type-tags)))))
+->
+  (let 
+    ((type-tags '(complex)))
+    (let 
+      ((proc (get 'magnitude type-tags)))
+      (if proc
+          (apply proc (cons 'rectangular (cons 3 4)))
+          (error
+            "No method for these types -- APPLY-GENERIC"
+            (list 'magnitude type-tags)))))
+->
+(apply
+  (get 'magnitude '(complex))
+  (cons 'rectangular (cons 3 4)))
+->
+(apply-generic 'magnitude (cons 'rectangular (cons 3 4)))
+->
+  (let 
+    ((type-tags (map type-tag (cons 'rectangular (cons 3 4)))))
+    (let 
+      ((proc (get 'magnitude type-tags)))
+      (if proc
+          (apply proc (map contents (cons 'rectangular (cons 3 4))))
+          (error
+            "No method for these types -- APPLY-GENERIC"
+            (list 'magnitude type-tags)))))
+->
+(apply 
+  (get 'magnitude (map type-tag (cons 'rectangular (cons 3 4))))
+  (map contents (cons 'rectangular (cons 3 4))))
+->
+(apply
+  (get 'magnitude '(rectangular))
+  '(3 4))
+->
+(apply
+  (lambda (z) 
+    (sqrt (+ (square (real-part z))
+             (square (imag-part z)))) )
+  '(3 4))
+->
+(sqrt (+ (square (real-part (3 4)))
+         (square (imag-part (3 4)))))
+->
+(sqrt (+ (square (car (3 4)))
+         (square (cdr (3 4)))))  
+->
+(sqrt (+ (square 3)
+         (square 4))
+->
+(sqrt (+ 9
+         16)  
+->
+(sqrt 25)
+-> 5
+
+-> 'apply-generic' is used twice:)
 |#
+
+; END
