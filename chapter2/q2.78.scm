bd771075b213656221288bb6c70e369ebe8e6d3a
naoiwata <nao@bascule.co.jp>
2013-04-14 01:08:55 +0900

    q2.78.scm modified solution

--- chapter2/q2.78.scm
+++ chapter2/q2.78.scm
@@ -5,19 +5,19 @@
 (define (type-tag datum)
   (cond
     ((pair? datum) (car datum))
-    ((number? datum) datum)
+    ((number? datum) 'scheme-number)
     (else
       (error "BAD TAGGED DATUM -- TYPE-TAG" datum))))
 
 (define (contents datum)
   (cond
-    ((pair? datum) (car datum))
+    ((pair? datum) (cdr datum))
     ((number? datum) datum)
     (else
       (error "BAD TAGGED DATUM -- CONTENTS" datum))))
 
 (define (attach-tag type-tag contents)
-  (if (number? contents)
+  (if (eq? type-tag 'scheme-number)
       contents
       (cons type-tag contents)))
 
