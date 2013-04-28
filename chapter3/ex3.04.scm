48eead7ebc046f8be30fb1e5d660e728bc4cf1dc
naoiwata <nao@bascule.co.jp>
2013-04-28 21:36:51 +0900

    3.04

---
+++ chapter3/ex3.04.scm
@@ -0,0 +1,46 @@
+;;
+;; @author naoiwata
+;; SICP Chapter3
+;; Exercise 3.4.
+;;
+
+(define (make-account balance password)
+  (define (widthdraw amount)
+    (if (>= balance amount)
+      (begin 
+        (set! balance (- balance amount))
+        balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (call-the-cops) (error "called 119 :("))
+  (define incorrect-types-num 0)
+  (define (dispatch p m)
+    (if (eq? p password)
+        (begin
+          (set! incorrect-types-num 0)
+          (cond
+            ((eq? m 'widthdraw) widthdraw)
+            ((eq? m 'deposit) deposit)
+            (else (error "Unknown request -- MEKE-AMOUNT" m))))
+        (begin
+          (if (> incorrect-types-num 6)
+              (call-the-cops)
+              (begin
+                (set! incorrect-types-num (+ incorrect-types-num 1))
+                (error "Incorrect password"))))))
+  dispatch)
+  
+; test
+(define acc (make-account 100 'panda))
+((acc 'panda 'widthdraw) 40)) ; 60
+((acc 'alpaca 'widthdraw) 40)
+((acc 'alpaca 'widthdraw) 40)
+((acc 'alpaca 'widthdraw) 40)
+((acc 'alpaca 'widthdraw) 40)
+((acc 'alpaca 'widthdraw) 40)
+((acc 'alpaca 'widthdraw) 40)
+((acc 'alpaca 'widthdraw) 40) ; called 119 :(
+
+; END
