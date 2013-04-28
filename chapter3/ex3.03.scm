91a7dc123dff4bf9837566a7481e96b5d9571adc
naoiwata <nao@bascule.co.jp>
2013-04-28 21:04:19 +0900

    3.03

---
+++ chapter3/ex3.03.scm
@@ -0,0 +1,31 @@
+;;
+;; @author naoiwata
+;; SICP Chapter3
+;; Exercise 3.3.
+;;
+
+(define (make-account balance password)
+  (define (widthdraw amount)
+    (if (>= balance amount)
+      (begin 
+        (set! balance (- balance amount))
+        balance)
+      "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (define (dispatch p m)
+    (if (eq? p password)
+        (cond
+          ((eq? m 'widthdraw) widthdraw)
+          ((eq? m 'deposit) deposit)
+          (else (error "Unknown request -- MEKE-AMOUNT" m)))
+        (else (error "Incorrect password" p))))
+  dispatch)
+
+; test
+(define acc (make-account 100 'panda))
+(print ((acc 'panda 'widthdraw) 40)) ; 40
+(print ((acc 'alpaca 'widthdraw) 40)) ; Incorrect password alpaca
+
+; END
