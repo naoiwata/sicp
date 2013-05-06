2e5d3c27bc23d2de5059d812d520b69c0c918043
naoiwata <nao@bascule.co.jp>
2013-05-06 11:22:16 +0900

    learned 3.2.1

---
+++ chapter3/pages/3.2.0.scm
@@ -0,0 +1,5 @@
+;;
+;; @author naoiwata
+;; SICP Chapter3
+;; 3.2 The Environment Model of Evaluation
+;;
---
+++ chapter3/pages/3.2.1.scm
@@ -0,0 +1,6 @@
+;;
+;; @author naoiwata
+;; SICP Chapter3
+;; 3.2.1 The Rules for Evaluation
+;;
+
