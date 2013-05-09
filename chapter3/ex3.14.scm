fd06f444134ffdf77c180cea463c1e2cbc4cffab
naoiwata <nao@bascule.co.jp>
2013-05-10 02:54:40 +0900

    ex3.14

---
+++ chapter3/ex3.14.scm
@@ -0,0 +1,21 @@
+;;
+;; @author naoiwata
+;; SICP Chapter3
+;; Exercise 3.14.
+;;
+
+(define (mystery x)
+  (define (loop x y)
+    (if (null? x)
+        y
+        (let
+          ((temp (cdr x)))
+          (set-cdr! x y)
+          (loop temp x))))
+  (loop x '()))
+
+(define W '(a b c d))
+(print W) ; (a b c d)
+(print (mystery '(a b c d))) ; (d c b a)
+
+; EMD
