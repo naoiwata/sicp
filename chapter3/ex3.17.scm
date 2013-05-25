84111da4ca1c66e974ef1b0cc127d996e05dd1ee
naoiwata <nao@bascule.co.jp>
2013-05-25 22:26:31 +0900

    chapeter3

--- chapter3/ex3.17.scm
+++ chapter3/ex3.17.scm
@@ -1,16 +1,21 @@
 ;;
 ;; @author naoiwata
 ;; SICP Chapter3
-;; Exercise 3.16.
+;; Exercise 3.17.
 ;;
 
-; Ben
-(define (count-pairs x)
-  (if (not (pair? x))
-      0
-      (+ (count-pairs (car x))
-         (count-pairs (cdr x))
-         1)))
+(define count-pairs
+  (let
+    ((counted '()))
+    (lambda (x)
+      (cond
+        ((not (pair? x)) 0)
+        ((memq x counted) 0)
+        (else
+          (set! counted (cons x counted))
+          (+ (count-pairs (car x))
+             (count-pairs (cdr x))
+             1))))))
 
 ; test
 (define list3 '(a b c))
@@ -32,8 +37,8 @@
 (set-cdr! (cdr (cdr linf)) linf)
  
 (print (count-pairs list3))   ; 3
-(print (count-pairs list4-1)) ; 4
-(print (count-pairs list7-1)) ; 5
-; (print (count-pairs linf))  ; F!!
+(print (count-pairs list4-1)) ; 3
+(print (count-pairs list7-1)) ; 3
+(print (count-pairs linf))    ; 3
 
 ; END
