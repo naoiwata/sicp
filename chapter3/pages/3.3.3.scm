99a67a669405023667d2ff6b74520d48c2739079
naoiwata <nao@bascule.co.jp>
2013-06-01 13:54:20 +0900

    learned Two-dimensional tables

--- chapter3/pages/3.3.3.scm
+++ chapter3/pages/3.3.3.scm
@@ -32,3 +32,73 @@
 (define (make-table)
   (list '*table*))
 
+; Two-dimensional tables ; 
+
+(define (lookup key-1 key-2 table)
+  (let 
+    ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let 
+          ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (cdr record)
+              #f))
+        #f)))
+
+(define (insert! key-1 key-2 value table)
+  (let 
+    ((subtable (assoc key-1 (cdr table))))
+    (if subtable
+        (let 
+          ((record (assoc key-2 (cdr subtable))))
+          (if record
+              (set-cdr! record value)
+              (set-cdr! subtable
+                        (cons (cons key-2 value)
+                              (cdr subtable)))))
+        (set-cdr! table
+                  (cons (list key-1
+                              (cons key-2 value))
+                        (cdr table)))))
+  'ok)
+
+; Creating local tables ; 
+
+(define (make-table)
+  (let
+    ((local-table (list '*table*)))
+    (define (lookup key-1 key-2 table)
+      (let 
+        ((subtable (assoc key-1 (cdr table))))
+        (if subtable
+            (let 
+              ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (cdr record)
+                  #f))
+            #f)))
+    (define (insert! key-1 key-2 value table)
+      (let 
+        ((subtable (assoc key-1 (cdr table))))
+        (if subtable
+            (let 
+              ((record (assoc key-2 (cdr subtable))))
+              (if record
+                  (set-cdr! record value)
+                  (set-cdr! subtable
+                            (cons (cons key-2 value)
+                                  (cdr subtable)))))
+            (set-cdr! table
+                      (cons (list key-1
+                                  (cons key-2 value))
+                            (cdr table)))))
+      'ok)
+    (define (dispatch m)
+      (cond
+        ((eq? m 'lookup-proc)  lookup)
+        ((eq? m 'insert-proc!) insert!)
+        (else
+          (error "Unknown table"))))
+    dispatch))
+
+; END
