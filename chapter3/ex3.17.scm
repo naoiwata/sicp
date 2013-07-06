582b9fba101a2250466d719a01f0cfcf589207e6
naoiwata <orrrizzle@gmail.com>
2013-07-06 23:56:32 +0900

    * modified solution

--- chapter3/ex3.17.scm
+++ chapter3/ex3.17.scm
@@ -41,4 +41,23 @@
 (print (count-pairs list7-1)) ; 3
 (print (count-pairs linf))    ; 3
 
+; ------------------------------------------------------
+; add
+; ------------------------------------------------------
+
+(define W1 '(a b c))
+(print W1) ; (a b c)
+(print (count-pairs W1)) ; 3
+
+(define W2-1 '(a))
+(define W2 (list W2-1 W2-1))
+(print W2) ; ((a) (a))
+(print (count-pairs W2)) ; 3
+
+(define W3-1 '(a))
+(define W3-2 (cons W3-1 W3-1))
+(define W3 (cons W3-2 W3-2))
+(print W3) ; (((a) a) (a) a)
+(print (count-pairs W3)) ; 3
+
 ; END
