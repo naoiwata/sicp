362e48bb99300a1360aff9372e7dad9ccc237626
naoiwata <orrrizzle@gmail.com>
2013-07-07 00:29:57 +0900

    * modified solution

--- chapter3/ex3.17.scm
+++ chapter3/ex3.17.scm
@@ -5,45 +5,17 @@
 ;;
 
 (define count-pairs
-  (let
-    ((counted '()))
+  (let 
+    ((counted '()))  
     (lambda (x)
-      (cond
-        ((not (pair? x)) 0)
-        ((memq x counted) 0)
-        (else
-          (set! counted (cons x counted))
-          (+ (count-pairs (car x))
-             (count-pairs (cdr x))
-             1))))))
-
-; test
-(define list3 '(a b c))
- 
-(define list4-1 '(b c))
-(define list4-2 '(a))
-(set-car! list4-1 list4-2) ; ((a) c)
-(set-car! (cdr list4-1) list4-2) ; ((a) (a))
- 
-(define list7-1 '(c))
-(define list7-2 '(b))
-(define list7-3 '(a))
-(set-car! list7-2 list7-3) ; ((a))
-(set-cdr! list7-2 list7-3) ; ((a) a)
-(set-car! list7-1 list7-2) ; (((a) a))
-(set-cdr! list7-1 list7-2) ; (((a) a) ((a) a))
- 
-(define linf '(a b c))
-(set-cdr! (cdr (cdr linf)) linf)
- 
-(print (count-pairs list3))   ; 3
-(print (count-pairs list4-1)) ; 3
-(print (count-pairs list7-1)) ; 3
-(print (count-pairs linf))    ; 3
-
-; ------------------------------------------------------
-; add
-; ------------------------------------------------------
+      (if (or (not (pair? x))
+              (memq x counted))
+          0
+          (begin
+            (set! counted (cons x counted))
+            (+ (count-pairs (car x))
+               (count-pairs (cdr x))
+               1))))))
 
 (define W1 '(a b c))
 (print W1) ; (a b c)
