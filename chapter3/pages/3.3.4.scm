54028c7c6e0f16c02d8e3ca57e375ec19ada4bb9
naoiwata <orrrizzle@gmail.com>
2013-07-10 02:04:07 +0900

    learned 3.3.4 representing wires

--- chapter3/pages/3.3.4.scm
+++ chapter3/pages/3.3.4.scm
@@ -71,4 +71,62 @@
   (add-action! a2 and-action-procedure)
   'ok)
 
+; ------------------------------------------------------------------------
+; Representing wires
+; ------------------------------------------------------------------------
+
+(define (make-wire)
+  (let
+    ((signal-value 0) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin
+            (set! signal-value new-value)
+            (call-each action-procedures))
+          'done)))
+  (define (accept-action-procedure! proc)
+    (set! action-procedures (cons proc action-procedures))
+    (proc))
+  (define (dispatch m)
+    (cond
+      ((eq? m 'get-signal)  signal-value)
+      ((eq? m 'set-signal)  set-my-signal!)
+      ((eq? m 'add-action!) accept-action-procedure!)
+      (else
+        (error "error:(" m)))))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+; ------------------------------------------------------------------------
+; The agenda
+; ------------------------------------------------------------------------
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let
+        ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
 ; END
