174dfd2b77ca783fbc83b847bd6e83e17933b7cc
naoiwata <orrrizzle@gmail.com>
2013-07-10 01:43:57 +0900

    ex3.30 but not test yet

--- chapter3/ex3.30.scm
+++ chapter3/ex3.30.scm
@@ -7,3 +7,20 @@
 ; ------------------------------------------------------------------------
 ; solution
 ; ------------------------------------------------------------------------
+
+(define (ripple-carry-adder ak bk sk ck)
+  (let
+    ((c (make-wire)))
+    (if (null? (cdr ak))
+        (set-signal! c 0)
+        (ripple-carry-adder (cdr ak) (cdr bk) (cdr sk) c))
+    (full-adder (car ak) (car bk) (car sk) ck)))
+
+;  = full-adder-delay * n 
+;  = (half-adder-delay * 2 + or-gate-delay) * n 
+;  = ((or-gate-delay + and-gate-delay + inverter-delay + and-gate-delay) + or-gate-delay) * n
+;  = (and-gate-delay * 4 + or-gate-delay * 3 + inverter-delay) * n
+
+; has not test yet:(
+
+; END
