f34a48ebde287c81d41d582df7df859d97441e58
naoiwata <orrrizzle@gmail.com>
2013-07-11 11:36:05 +0900

    * modified and bug fixed as test enable to be evaled

--- chapter3/ex3.32.scm
+++ chapter3/ex3.32.scm
@@ -50,20 +50,154 @@
       (cond
         ((eq? m 'insert-queue!) insert-queue!)
         ((eq? m 'delete-queue!) (delete-queue!))
+        ((eq? m 'front-queue)   front-queue)
+        ((eq? m 'empty-queue?)  empty-queue?)
         (else
           (error "Undefined operation -- MAKE-QUEUE" m))))
     dispatch))
 
 (define (insert-queue! queue item) 
   ((queue 'insert-queue!) item))
+
 (define (delete-queue! queue) 
   (queue 'delete-queue!))
+
+(define (front-queue queue) 
+  (queue 'front-queue))
+
+(define (empty-queue? queue) 
+  (queue 'empty-queue?))
 
 ; ------------------------------------------------------------------------
 ; import 3.3.4
 ; ------------------------------------------------------------------------
 
+; several gates
+(define (inverter input output)
+  (define (invert-input)
+    (let 
+      ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else 
+          (error "Invalid signal" s))))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let 
+      ((new-value
+         (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define (logical-and s c)
+  (cond ((and (= s 1) (= c 1)) 1)
+        ((or
+           (and (= s 0) (= c 0))
+           (and (= s 1) (= c 0))
+           (and (= s 0) (= c 1))) 0)
+        (else 
+          (error "Invalid signal" s))))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((not-a1 (make-wire))
+          (not-a2 (make-wire))
+          (b (make-wire)))
+      (inverter a1 not-a1)
+      (inverter a2 not-a2)
+      (and-gate not-a1 not-a2 b)
+      (inverter b output)))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+; half-addr
+(define (half-adder a b s c)
+  (let 
+    ((d (make-wire)) 
+     (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+; Representing wires
+(define (make-wire)
+  (let 
+    ((signal-value 0) 
+     (action-procedures '()))
+  (define (set-my-signal! new-value)
+    (if (not (= signal-value new-value))
+        (begin (set! signal-value new-value)
+          (call-each action-procedures))
+        'done))
+  (define (accept-action-procedure! proc)
+    (set! action-procedures (cons proc action-procedures))
+    (proc))
+  (define (dispatch m)
+    (cond ((eq? m 'get-signal) signal-value)
+          ((eq? m 'set-signal!) set-my-signal!)
+          ((eq? m 'add-action!) accept-action-procedure!)
+          (else 
+            (error "Unknown operation" m))))
+  dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
 ; Implementing the agenda
+(define (after-delay delay action)
+  (add-to-agenda! 
+    (+ delay (current-time the-agenda))
+    action
+    the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let 
+        ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display " New-value = ")
+                 (display (get-signal wire)))))
+
+; A sample simulation
 (define (make-time-segment time queue)
   (cons time queue))
 
@@ -142,128 +276,6 @@
         (set-current-time! agenda (segment-time first-seg))
         (front-queue (segment-queue first-seg)))))
 
-(define (probe name wire)
-  (add-action! wire
-               (lambda ()
-                 (newline)
-                 (display name)
-                 (display " ")
-                 (display (current-time the-agenda))
-                 (display " New-value = ")
-                 (display (get-signal wire)))))
-
-(define (propagate)
-  (if (empty-agenda? the-agenda)
-      'done
-      (let 
-        ((first-item (first-agenda-item the-agenda)))
-        (first-item)
-        (remove-first-agenda-item! the-agenda)
-        (propagate))))
-
-(define (after-delay delay action)
-  (add-to-agenda! 
-    (+ delay (current-time the-agenda))
-    action
-    the-agenda))
-
-(define (get-signal wire)
-  (wire 'get-signal))
-
-(define (set-signal! wire new-value)
-  ((wire 'set-signal!) new-value))
-
-(define (add-action! wire action-procedure)
-  ((wire 'add-action!) action-procedure))
-
-(define (call-each procedures)
-  (if (null? procedures)
-      'done
-      (begin
-        ((car procedures))
-        (call-each (cdr procedures)))))
-
-; Representing wires
-(define (make-wire)
-  (let 
-    ((signal-value 0) (action-procedures '()))
-  (define (set-my-signal! new-value)
-    (if (not (= signal-value new-value))
-        (begin (set! signal-value new-value)
-          (call-each action-procedures))
-        'done))
-  (define (accept-action-procedure! proc)
-    (set! action-procedures (cons proc action-procedures))
-    (proc))
-  (define (dispatch m)
-    (cond ((eq? m 'get-signal) signal-value)
-          ((eq? m 'set-signal!) set-my-signal!)
-          ((eq? m 'add-action!) accept-action-procedure!)
-          (else 
-            (error "Unknown operation" m))))
-  dispatch))
-
-; half-addr
-(define (half-adder a b s c)
-  (let 
-    ((d (make-wire)) (e (make-wire)))
-    (or-gate a b d)
-    (and-gate a b c)
-    (inverter c e)
-    (and-gate d e s)
-    'ok))
-
-; several gates
-(define (inverter input output)
-  (define (invert-input)
-    (let 
-      ((new-value (logical-not (get-signal input))))
-      (after-delay inverter-delay
-                   (lambda ()
-                     (set-signal! output new-value)))))
-  (add-action! input invert-input)
-  'ok)
-
-(define (logical-not s)
-  (cond ((= s 0) 1)
-        ((= s 1) 0)
-        (else 
-          (error "Invalid signal" s))))
-
-(define (and-gate a1 a2 output)
-  (define (and-action-procedure)
-    (let 
-      ((new-value
-         (logical-and (get-signal a1) (get-signal a2))))
-      (after-delay and-gate-delay
-                   (lambda ()
-                     (set-signal! output new-value)))))
-  (add-action! a1 and-action-procedure)
-  (add-action! a2 and-action-procedure)
-  'ok)
-
-(define (logical-and s c)
-  (cond ((and (= s 1) (= c 1)) 1)
-        ((or
-           (and (= s 0) (= c 0))
-           (and (= s 1) (= c 0))
-           (and (= s 0) (= c 1))) 0)
-        (else 
-          (error "Invalid signal" s))))
-
-(define (or-gate a1 a2 output)
-  (define (or-action-procedure)
-    (let ((not-a1 (make-wire))
-          (not-a2 (make-wire))
-          (b (make-wire)))
-      (inverter a1 not-a1)
-      (inverter a2 not-a2)
-      (and-gate not-a1 not-a2 b)
-      (inverter b output)))
-  (add-action! a1 or-action-procedure)
-  (add-action! a2 or-action-procedure)
-  'ok)
-
 ; ------------------------------------------------------------------------
 ; test
 ; ------------------------------------------------------------------------
@@ -278,10 +290,11 @@
 (define sum (make-wire))
 (define carry (make-wire))
 
-(probe 'sum sum) ; sum 0 New-value = 0 
+(probe 'sum sum)     ; sum 0 New-value = 0 
 (probe 'carry carry) ; carry 0 New-value = 0
+
 (half-adder input-1 input-2 sum carry) ; ok
-(set-signal! input-1 1) ; done
-(propagate) ; ERROR
+(set-signal! input-1 1)                ; done
+(propagate)                            ; done
 
 ; END
