57fbd937e288a5ead28925d85052974c890d6ee0
naoiwata <orrrizzle@gmail.com>
2013-07-14 01:39:05 +0900

    learned Representing connectors

--- chapter3/pages/3.3.5.scm
+++ chapter3/pages/3.3.5.scm
@@ -27,7 +27,7 @@
     (constant 32 y)))
 
 ; ------------------------------------------------------------------------
-; Representing connectors
+; Implementing the constraint system
 ; ------------------------------------------------------------------------
 
 ; adder
@@ -138,4 +138,77 @@
   (connect connector me)
   me)
 
+; ------------------------------------------------------------------------
+; Representing connectors
+; ------------------------------------------------------------------------
 
+(define (make-conector)
+  (let
+    ((value #f)
+     (informant #f)
+     (constraints '()))
+    (define (set-my-value newval setter)
+      (cond
+        ((not (has-value? me))
+         (set! value newval)
+         (set! informant setter)
+         (for-each-except setter
+                          inform-about-value
+                          constraints))
+        ((not (= value newval))
+         (error "contradiction" (list value newval)))
+        (else
+          'ignored)))
+    (define (forget-my-value retractor)
+      (if (eq? retractor informant)
+          (begin 
+            (set! informant #f)
+            (for-each-except retractor
+                             inform-about-no-value
+                             constraints))
+          'ignored))
+    (define (connect new-constraint)
+      (if (not (memq new-constraint constraints))
+          (set! constraints
+                (cons new-constraint constraints)))
+      (if (has-value? me)
+          (inform-about-value new-constraint))
+      'done)
+    (define (me request)
+      (cond
+        ((eq? request 'has-value?)
+         (if informant #t #f))
+        ((eq? request 'value)      value)
+        ((eq? request 'set-value!) set-my-value)
+        ((eq? request 'forget)     forget-my-value)
+        ((eq? request 'connect)    connect)
+        (else
+          (error "unknown operation" request))))
+    me))
+
+(define (for-each-except exception procedure list)
+  (define (loop items)
+    (cond
+      ((null? items) 'dome)
+      ((eq? (car items) exception) (loop (cdr items)))
+      (else
+        (procedure (car items))
+        (loop (cdr items)))))
+  (loop list))
+
+(define (has-value? connector)
+  (connector 'has-value?))
+
+(define (get-value connector)
+  (connector 'get-value))
+
+(define (set-value! connector new-value informant)
+  ((connector 'set-value!) new-value informant))
+
+(define (forget-value! connector retractor)
+  ((connector 'forget-value!) retractor))
+
+(define (connect connector new-constraint)
+  ((connector 'connect) new-constraint))
+
+; END
