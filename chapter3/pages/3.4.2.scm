a213537c593a3d3431c7d2f8a1b3b994f3ad59a9
naoiwata <orrrizzle@gmail.com>
2013-07-20 02:25:18 +0900

    3.46

--- chapter3/pages/3.4.2.scm
+++ chapter3/pages/3.4.2.scm
@@ -84,4 +84,41 @@
      account1
      account2)))
 
+; ------------------------------------------------------------------------
+; Implementing serializers
+; ------------------------------------------------------------------------
+
+(define (make-serializer)
+  (let 
+    ((mutex (make-mutex)))
+    (lambda (p)
+      (define (serialized-p . args)
+        (mutex 'acquire)
+        (let 
+          ((val (apply p args)))
+          (mutex 'release)
+          val))
+      serialized-p)))
+
+(define (make-mutex)
+  (let 
+    ((cell (list #f)))
+    (define (the-mutex m)
+      (cond 
+        ((eq? m 'acquire)
+         (if (test-and-set! cell)
+             (the-mutex 'acquire))) ; retry
+        ((eq? m 'release) (clear! cell))))
+    the-mutex))
+
+(define (clear! cell)
+  (set-car! cell false))
+
+(define (test-and-set! cell)
+  (if (car cell)
+      #t
+      (begin 
+        (set-car! cell true)
+        #f)))
+
 ; END
