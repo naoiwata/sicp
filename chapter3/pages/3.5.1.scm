bae448419636332c54d1d60af16527cc36d7b6a1
naoiwata <orrrizzle@gmail.com>
2013-07-23 02:59:15 +0900

    stream..

--- chapter3/pages/3.5.1.scm
+++ chapter3/pages/3.5.1.scm
@@ -52,3 +52,70 @@
   (newline)
   (display x))
 
+; (cons-stream <a> <b>) == (cons <a> (delay <b>))
+
+(define (stream-car stream)
+  (car stream))
+
+(define (stream-cdr stream)
+  (force (cdr stream)))
+
+(stream-car
+  (stream-cdr
+    (stream-filter prime?
+                   (stream-enumerate-interval 10000 1000000))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+        low
+        (stream-enumerate-interval (+ low 1) high))))
+
+(cons 10000
+      (delay (stream-enumerate-interval 10001 1000000)))
+
+(cons-stream (stream-car stream)
+             (stream-filter pred (stream-cdr stream)))
+
+(cons 10007
+      (delay
+        (stream-filter
+          prime?
+          (cons
+            10008
+            (delay 
+              (stream-enumerate-interval 10009 1000000))))))
+
+(cons 10009
+      (delay 
+        (stream-filter
+          prime?
+          (cons 10010
+                (delay 
+                  (stream-enumerate-interval 10011 1000000))))))
+
+; ------------------------------------------------------------------------
+; Implementing delay and force
+; ------------------------------------------------------------------------
+
+; (delay <exp>) == (lambda () <exp>)
+
+(define (delay delayed-object)
+  (lambda () delayed-object))
+
+(define (force delayed-object)
+  (delayed-object))
+
+(define (memo-proc proc)
+  (let 
+    ((already-run? #f)
+     (result #f))
+    (lambda ()
+      (if (not already-run?)
+          (begin
+            (set! result (proc))
+            (set! already-run? #t))
+          result))))
+
+; END
