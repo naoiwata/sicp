1e5093b22e624fd5b7f4e29c06a2e59f88766d63
naoiwata <orrrizzle@gmail.com>
2013-08-28 02:57:07 +0900

    pairs

--- chapter3/pages/3.5.3.scm
+++ chapter3/pages/3.5.3.scm
@@ -133,3 +133,34 @@
 ; 3.141592653589778
 ; 3.1415926535897953
 ; 3.141592653589795
+
+; ------------------------------------------------------------------------
+; Infinite streams of pairs
+; ------------------------------------------------------------------------
+
+(stream-filter
+  (lambda (pair)
+    (prime? (+ (car pair) (cadr pair))))
+  (int-pairs))
+
+(stream-map
+  (lambda (x)
+    (list ((stream-car s) x)))
+  (stream-cdr t))
+
+(define (interleave s1 s2)
+  (if (stream-null? s1)
+      s2
+      (cons-stream (stream-car s1)
+                   (interleave s2 (stream-cdr s1)))))
+
+(define (pairs s t)
+  (cons-stream
+    (list (stream-car s) (stream-car t))
+    (interleave
+      (stream-map
+        (lambda (x) (list (stream-car s) x))
+        (stream-cdr t))
+      (pairs (stream-cdr s) (stream-cdr t)))))
+
+
