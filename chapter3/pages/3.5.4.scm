f2686003dd92bab3eb5a9d743b165c0c2ae6ac14
naoiwata <orrrizzle@gmail.com>
2013-09-03 02:34:01 +0900

    delayed argument

--- chapter3/pages/3.5.4.scm
+++ chapter3/pages/3.5.4.scm
@@ -4,3 +4,26 @@
 ;; 3.5.4 Streams and Delayed Evaluation
 ;;
 
+(define int
+  (cons-stream
+    initial-value
+    (add-streams (scale-stream integrand dt)
+                 int)))
+
+(define (solve f y0 dt)
+  (define y (integral dy y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream
+      initial-value
+      (add-streams (scale-stream integrand dt)
+                   int)))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
