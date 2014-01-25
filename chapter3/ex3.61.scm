b27e155704c4aa2073cb7b2d949b6325f9efc056
naoiwata <orrrizzle@gmail.com>
2014-01-25 15:41:41 +0900

    Add test for ex3.61.scm

--- chapter3/ex3.61.scm
+++ chapter3/ex3.61.scm
@@ -13,9 +13,38 @@
 
 (define (invert-unit-series S)
   (cons-stream 1
-               (scale-stream
-                 (mul-series (stream-cdr S)
-                             (invert-unit-series S))
-                 -1)))
+               (stream-map -
+                           (mul-series (stream-cdr S)
+                                       (invert-unit-series S)))))
+
+; ------------------------------------------------------------------------
+; test
+; ------------------------------------------------------------------------
+
+(define ones (cons-stream 1 ones))
+(define (add-streams s1 s2) (stream-map + s1 s2))
+(define integers (cons-stream 1 (add-streams ones integers)))
+(define (mul-streams s1 s2) (stream-map * s1 s2))
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+(define (mul-series s1 s2)
+  (cons-stream (* (stream-car s1) (stream-car s2))
+               (add-stream
+                 (stream-map
+                   (lambda (k) (* (stream-car s1) x))
+                   (stream-car s2))
+                 (mul-series (stream-cdr s1) s2))))
+
+(print
+   (stream-ref
+     (invert-unit-series integers) 0))
+; 1
+
+(print
+   (stream-ref
+     (invert-unit-series integers) 1))
+; -2
+
+; TODO:
 
 ; END
