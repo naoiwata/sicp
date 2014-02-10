5767e2b90f5fc3f5511bf23a45e931c1e9163a64
naoiwata <orrrizzle@gmail.com>
2014-02-10 17:11:56 +0900

    Modify

--- chapter3/ex3.67.scm
+++ chapter3/ex3.67.scm
@@ -22,12 +22,12 @@
     (list (stream-car s) (stream-car t))
     (interleave
       (interleave
-          (stream-map
-            (lambda (x) (list (stream-car s) x))
-            (stream-cdr t))
-          (stream-map
-            (lambda (x) (list x (stream-car s)))
-            (stream-cdr t)))
+        (stream-map
+          (lambda (x) (list (stream-car s) x))
+           (stream-cdr t))
+        (stream-map
+          (lambda (x) (list (stream-car t) x))
+           (stream-cdr s)))
       (pairs (stream-cdr s) (stream-cdr t)))))
 
 ; ------------------------------------------------------------------------
