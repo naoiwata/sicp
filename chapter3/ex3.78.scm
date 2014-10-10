1c536d71c9fe43e7167ff84cf0530940f1753c1d
naoiwata <orrrizzle@gmail.com>
2014-10-11 00:08:42 +0900

    Solved 3.78

---
+++ chapter3/ex3.78.scm
@@ -0,0 +1,32 @@
+;;
+;; @author naoiwata
+;; SICP Chapter3
+;; Exercise 3.78.
+;;
+
+; ------------------------------------------------------------------------
+; question
+; ------------------------------------------------------------------------
+
+(add-load-path "./pages/" :relative)
+(load "stream.scm")
+
+(define (add-streams s1 s2)
+  (stream-map + s1 s2))
+
+(define (scale-stream stream factor)
+  (stream-map (lambda (x) (* x factor)) stream))
+
+(define (integral integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+                 (add-streams (scale-stream integrand dt)
+                              int)))
+  int)
+
+(define (solve-2nd a b dt y0 dy0)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (integral (delay ddy) dy0 dt))
+  (define ddy (add-streams (scale-stream dy a)
+                           (scale-stream y b)))
+  y)
