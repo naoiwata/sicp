5b9c966d6f0f9024889fb16646756e908641b71a
naoiwata <orrrizzle@gmail.com>
2014-10-13 22:32:40 +0900

    4.1.1

---
+++ chapter4/pages/4.1.1.scm
@@ -0,0 +1,70 @@
+;;
+;; @author naoiwata
+;; SICP Chapter4
+;; 4.1.1 The Core of the Evaluator
+;;
+
+(define (eval exp env)
+  (cond
+    ((self-evalutating? exp) exp)
+    ((variable? exp) (lookup-variable-value exp env))
+    ((quoted? exp) (text-of-quotation exp))
+    ((assignment? exp) (eval-assignment exp env))
+    ((definition? exp) (eval-definition exp env))
+    ((if? exp) (eval-if exp env))
+    ((lambda? exp)
+     (make-procedure (lambda-rparameters exp)
+                     (lambda-body exp)
+                     env))
+    ((begin? exp)
+     (eval-sequence (lambda-actions exp) env))
+    ((cond? exp) (eval (cond->if exp) env))
+    ((application? exp)
+     (apply (eval (operator exp) env)
+            (list-of-values (operands exp) env)))
+    (else
+      (error "Unknown expression type -- EVAL" exp))))
+
+(define (apply procedure arguments)
+  (cond 
+    ((primitive-procedure? procedure)
+     (apply-primitive-procedure procedure arguments))
+    ((compound-procedure? procedure)
+     (eval-sequence
+       (procedure-body procedure)
+       (extend-environment
+         (procedure-parameters procedure)
+         arguments
+         (procedure-environment procedure))))
+    (else
+      (error "Unknown procedure type -- APPLY" procedure))))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (cons (eval (first-operand exps) env)
+            (list-of-values (rest-operands exps) env))))
+
+(define (eval-if exp env)
+  (if (true? (eval (if-predicate exp) env))
+      (eval (if-consequent exp) env)
+      (eval (if-alternative exp) env)))
+
+(define (eval-sequence exps env)
+  (cond
+    ((last-exp? exps) (eval (first-exp exps) env))
+    (else
+      (eval (first-exp exps) env)
+      (eval-sequence (rest-exp exps) env))))
+
+(define (eval-assignment exp env)
+  (set-variable-value! (assignment-variable exp)
+                       (eval (assignment-variable exp) env)
+                       env)
+  'ok)
+
+(define (eval-definition exp env)
+  (define-value! (definition-variable exp)
+                       (eval (definition-variable exp) env)
+                       env)
+  'ok)
