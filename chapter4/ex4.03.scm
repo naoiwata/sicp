0b25b2d181fb8f6c095d96373b28296c08a145fa
naoiwata <orrrizzle@gmail.com>
2014-10-18 22:56:45 +0900

    Solved ex4.3

--- chapter4/ex4.03.scm
+++ chapter4/ex4.03.scm
@@ -8,10 +8,58 @@
 ; solution
 ; ------------------------------------------------------------------------
 
-(define deriv-table (make-hash-table))
+(add-load-path "./" :relative)
+(load "pages/4.1.1.scm")
+(load "pages/4.1.2.scm")
+
+(define eval-table (make-hash-table))
 
 (define (put op type item)
-  (hash-table-put! deriv-table type item))
+  (hash-table-put! eval-table type item))
 
 (define (get op type)
-  (hash-table-get deriv-table type))
+  (hash-table-get eval-table type))
+
+(define (eval exp env)
+  (cond
+    ((self-evaluating? exp) exp)
+    ((variable? exp) (lookup-variable-value exp env))
+    ((get (car exp))
+     ((get (car exp)) exp env))
+    ((application? exp)
+     (apply (eval (operator exp) env)
+            (list-of-values (operands exp) env)))
+    (else
+      (error "Unknown expression type -- EVAL" exp))))
+
+(put eval-table
+     'quote 
+     (lambda (exp env)
+       (text-of-quotation exp)))
+
+(put eval-table
+     'set!
+     (lambda (exp env)
+       (eval-assignment exp env)))
+
+(put eval-table
+     'define eval-definition)
+
+(put eval-table 'if eval-if)
+
+(put eval-table
+     'lambda 
+     (lambda (exp env)
+       (make-procedure (lambda-parameters exp)
+                       (lambda-body exp)
+                       env)))
+
+(put eval-table
+     'begin
+     (lambda (exp env)
+       (eval-sequence (begin-actions exp) env)))
+
+(put eval-table
+     'cond
+     (lambda (exp env)
+        (eval (cond->if exp) env)))
