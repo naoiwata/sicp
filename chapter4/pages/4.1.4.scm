4f20ebe599aa8aadccfa907f79b886d2d90c4c9e
naoiwata <orrrizzle@gmail.com>
2014-11-12 23:44:00 +0900

    add setup-environment

--- chapter4/pages/4.1.4.scm
+++ chapter4/pages/4.1.4.scm
@@ -3,3 +3,15 @@
 ;; SICP Chapter4
 ;; 4.1.4  Running the Evaluator as a Program
 ;;
+
+(define (setup-environment)
+  (let ((initial-env
+         (extend-environment (primitive-procedure-names)
+                             (primitive-procedure-objects)
+                             the-empty-environment)))
+    (define-variable! '#t #t initial-env)
+    (define-variable! '#f #f initial-env)
+    initial-env))
+
+(define the-global-environment (setup-environment))
+
