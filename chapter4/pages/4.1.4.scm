2458cbae43a1bf170e50e24c98e3f03e0d2d8947
naoiwata <orrrizzle@gmail.com>
2014-11-17 22:43:27 +0900

    4.1.4

--- chapter4/pages/4.1.4.scm
+++ chapter4/pages/4.1.4.scm
@@ -43,3 +43,34 @@
 
 (define input-prompt ";;; M-Eval input:")
 (define output-prompt ";;; M-Eval output:")
+
+(define (deriver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (announce-output output-prompt)
+      (user-print output)))
+  (driver-loop))
+
+(define (prompt-for-input string)
+  (newline)
+  (newline)
+  (display string)
+  (newline))
+
+(define (announce-output string)
+  (newline)
+  (display string)
+  (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-parameters
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '<procedure-env>))
+      (display object)))
+
+(define the-global-environment (setup-environment))
+
+(deriver-loop)
